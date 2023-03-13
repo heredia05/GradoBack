fa53c7f1c240a0cf10abc3f230a9362bdf4a1b4e
heredia05 <esteban.herediab@konradlorenz.edu.co>
2023-03-12 20:12:33 -0500

    First commit

---
+++ prototype/src/test/java/com/konrad/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
